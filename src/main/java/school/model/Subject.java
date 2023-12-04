e842d72561a997fbb17efe8487ec6510d375721e
Richard Poroszlay <richardporoszlay@gmail.com>
2023-12-04 17:34:54 +0100

    Add relationship annotation to the Subject entity

--- src/main/java/school/model/Subject.java
+++ src/main/java/school/model/Subject.java
@@ -1,9 +1,6 @@
 package school.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,5 +19,11 @@ public class Subject {
     private Long id;
     private String name;
 
+    @ManyToMany
+    @JoinTable(
+            name = "student_enrolled",
+            joinColumns = @JoinColumn(name = "subject_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
     private Set<Student> enrolledStudents = new HashSet<>();
 }
