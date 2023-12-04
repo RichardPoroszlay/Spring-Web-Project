68a677a58ca6d0b1e126be9c1bcbe225d364ada0
Richard Poroszlay <richardporoszlay@gmail.com>
2023-12-04 18:56:08 +0100

    Create SubjectController

---
+++ src/main/java/school/controller/SubjectController.java
@@ -0,0 +1,67 @@
+package school.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.dto.SubjectDto;
+import school.model.Subject;
+import school.service.StudentService;
+import school.service.SubjectService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/subjects")
+public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    private final StudentService studentService;
+
+    @Autowired
+    public SubjectController(SubjectService subjectService, StudentService studentService) {
+        this.subjectService = subjectService;
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/add-subject")
+    public SubjectDto newSubject(@RequestBody SubjectDto newSubjectDto) {
+        return subjectService.addSubject(newSubjectDto);
+    }
+
+    @GetMapping("/get-subjects")
+    public List<SubjectDto> getAllSubjects() {
+        return subjectService.getAllSubjects();
+    }
+
+    @DeleteMapping("/delete-subject/{id}")
+    public ResponseEntity<String> deleteSubject(@PathVariable Long id) {
+        if(subjectService.deleteSubject(id)) {
+            return ResponseEntity.ok("Subject deleted with the id: " + id);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no subject with id: " + id);
+        }
+    }
+
+    @PutMapping("/edit-subject/{id}")
+    public ResponseEntity<String> editSubject(@PathVariable Long id, @RequestBody Subject editedSubject) {
+        if(subjectService.editSubject(id, editedSubject)) {
+            return ResponseEntity.ok("Subject edited with the id: " + id);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no subject with id: " + id);
+        }
+    }
+
+    @GetMapping("subject/{id}")
+    public ResponseEntity<?> getSubjectById(@PathVariable Long id) {
+        Optional<SubjectDto> subjectDto = subjectService.getSubjectById(id);
+
+        if(subjectDto.isPresent()) {
+            return ResponseEntity.ok(subjectDto.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no subject with id: " + id);
+        }
+    }
+}
