da38803de9fc038784c1805564a1eebd9b4e3319
Richard Poroszlay <richardporoszlay@gmail.com>
2023-12-05 22:04:21 +0100

    Create outer src files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {
+  DefaultNavbar,
+  StudentTable,
+  Hero,
+  SubjectTable,
+  AddStudent,
+  EditStudent,
+  AddSubject,
+  EditSubject,
+  PreviewSubject,
+} from "./components";
+import PreviewFruit from "./components/PreviewStudent";
+
+function App() {
+  return (
+    <div>
+      <DefaultNavbar></DefaultNavbar>
+      <Routes>
+        <Route path="" element={<Hero />} />
+        <Route path="/students" element={<StudentTable />} />
+        <Route path="/subjects" element={<SubjectTable />} />
+        <Route path="/add-student" element={<AddStudent />} />
+        <Route path="/edit-student/:id" element={<EditStudent />} />
+        <Route path="/add-subject" element={<AddSubject />} />
+        <Route path="/edit-subject/:id" element={<EditSubject />} />
+        <Route path="/student/:id" element={<PreviewStudent />} />
+        <Route path="/subject/:id" element={<PreviewSubject />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
