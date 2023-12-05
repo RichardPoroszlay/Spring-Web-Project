096cf843faa9dfdfc69b7e29e67a7384d86c5d7a
Richard Poroszlay <richardporoszlay@gmail.com>
2023-12-05 21:56:15 +0100

    Create index.js

---
+++ frontend/src/components/index.js
@@ -0,0 +1,23 @@
+import StudentTable from "./StudentTable";
+import DefaultNavbar from "./DefaultNavbar";
+import Hero from "./Hero";
+import SubjectTable from "./SubjectTable";
+import AddStudent from "./AddStudent";
+import EditStudent from "./EditStudent";
+import AddSubject from './AddSubject';
+import EditSubject from "./EditSubject";
+import PreviewStudent from "./PreviewStudent";
+import PreviewSubject from "./PreviewSubject";
+
+export{
+    StudentTable,
+    DefaultNavbar,
+    Hero,
+    SubjectTable,
+    AddStudent,
+    EditStudent,
+    AddSubject,
+    EditSubject,
+    PreviewStudent,
+    PreviewSubject
+};
